3e5c68862d53783bc7f1f94fac17adfcb770747a
danny-yamamoto <yamamoto_daisuke@retail-ai.jp>
2023-09-25 05:00:27 +0000

    Add experience section

---
+++ src/pages/experience.tsx
@@ -0,0 +1,11 @@
+import { Layout } from "../components/Layout"
+
+export const Experience = () => {
+    return (
+        <Layout title="">
+        <main>
+            <h1>hoge</h1>
+        </main>
+        </Layout>
+    )
+}
